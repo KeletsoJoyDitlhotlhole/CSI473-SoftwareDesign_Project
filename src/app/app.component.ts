b04d4b075cac9e94025d90d9c9f1fb90d1f4af24
Trudy Dube <trudydube@gmail.com>
2024-10-18 10:33:04 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'csi473-project-phase2-ref3-4';
+}
